2fde0bbd542ea3594d44461d95db4e6790d7f471
pakuyuya <pakuyuya@yahoo.co.jp>
2019-08-01 23:43:35 +0900

    Top画面作りこみ中

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,37 +1,58 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+    <v-navigation-drawer absolute>
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              S3 Web Browser
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+    </v-navigation-drawer>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <v-divider></v-divider>
+
+    <v-list dense class="pt-0">
+      <v-list-tile v-for="profile in profiles" :key="profile.id" @click="selectProfile">
+        <v-list-tile-action>
+          <v-icon>cloud_circle</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-component>
+          <v-list-tile-title>{{ profile.name }}</v-list-tile-title>
+        </v-list-tile-component>
+      </v-list-tile>
+    </v-list>
+
+    <v-container>
+      <v-layout justify-center v-if="selectedProfile === undefined" >
+        接続先を選択してください。
+      </v-layout>
+      <v-list v-if="selectedProfile !== undefined" class="text-xs-center">
+        <s3list />
+      </v-list>
+    </v-container>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import S3list from './components/S3list.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld,
+    S3list,
   },
   data: () => ({
-    //
+    profiles : [],
+    selectedProfile: undefined,
   }),
+  methods: {
+    selectProfile () {
+
+    },
+  },
 });
 </script>
